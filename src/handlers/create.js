e2d92f084b2693e291d9defdceb8238c7263f1ab
nadtakanjones <nadtakan.jones@gmail.com>
2021-07-28 13:19:49 -0700

    added sqs

--- src/handlers/create.js
+++ src/handlers/create.js
@@ -1,20 +1,11 @@
 'use strict';
-const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb")
-const client = new DynamoDBClient({ region: process.env.REGION })  
-const util = require('../helper/util') 
+// const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb")
+// const client = new DynamoDBClient({ region: process.env.REGION })  
 const response = require('../helper/response-lib')
-const sns = require('../helper/sns')
 
-module.exports.handler = async event => {
-	try {
-        const { body } = event.detail;
-        const { claims } = event.detail.requestContext.authorizer;
-
-        const params = util.formatItem(JSON.parse(body), claims)
-        const command = new PutItemCommand(params);
-        const metadata = await client.send(command); 
-        return response.success(metadata.$metadata.httpStatusCode)
-    } catch (err) {
-        await sns.notifyFailure(err);
-	}
+module.exports.queue = async event => {
+	
 };
+
+module.exports.process = async event => {
+}
