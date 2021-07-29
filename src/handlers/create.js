892a8cdecf551689f96dc014ddce9e67f15cbc00
nadtakanjones <nadtakan.jones@gmail.com>
2021-07-28 21:53:57 -0700

    added lambda role, sqs permission

--- src/handlers/create.js
+++ src/handlers/create.js
@@ -6,24 +6,29 @@ const client = new DynamoDBClient({ region: process.env.REGION })
 const util = require('../helper/util') 
 
 module.exports.queue = async event => {
-	const params = {
-        MessageBody: JSON.stringify(event.body),
-        QueueUrl: process.env.SOURCE_QUEUE_URL
-    }
-    const response = await sqs.sendCommand(params);
-    return {
-        statusCode: 200,
-        body: JSON.stringify(response)
+	try {
+        const params = {
+            MessageBody: JSON.stringify(event.body),
+            QueueUrl: process.env.SOURCE_QUEUE_URL
+        }
+        const response = await sqs.sendCommand(params);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(response)
+        }
+    } catch (e) {
+        console.log(e)
     }
 };
 
 module.exports.process = async event => {
-    console.log(event.Records[0].body)
-
-    const { body } = event.Records[0];
-
-    const params = util.formatItem(JSON.parse(body))
-    const command = new PutItemCommand(params);
-    const metadata = await client.send(command); 
+    try {
+        const { body } = event.Records[0];
+        const params = util.formatItem(JSON.parse(body))
+        const command = new PutItemCommand(params);
+        await client.send(command); 
+    } catch (e) {
+        console.log(e)
+    }
     
 }
--- src/helper/util.js
+++ src/helper/util.js
@@ -5,18 +5,21 @@ const tableName = process.env.TABLE_NAME;
 const ulid = require('ulid');
 
 util.formatItem = ( gopher => {
+
+    const { name, gopherStatus, gopherType, location } = JSON.parse(gopher);
     const id = ulid.ulid();
-    let name = gopher.name.toLowerCase();
+
+    const newName = name.toLowerCase();
     return {
         TableName: tableName,
         Item: {
-            PK: { S : `PLAYER#${name.replace(/\s/g, '')}` },
+            PK: { S : `PLAYER#${newName.replace(/\s/g, '')}` },
             SK: { S: `PLAYERID#${id}` },
             id: { S: id },
-            name: { S: name },
-            gopherStatus: { S: gopher.gopherStatus.toLowerCase() },
-            location: { S: gopher.location.toLowerCase() },
-            gopherType: { S: gopher.gopherType.toLowerCase() },
+            name: { S: newName },
+            gopherStatus: { S: gopherStatus.toLowerCase() || '' },
+            location: { S: location.toLowerCase() || '' },
+            gopherType: { S: gopherType.toLowerCase() || '' },
             userStatus: { S: "player" } ,
             createdAt: { S: Date.now().toString() }
         },
